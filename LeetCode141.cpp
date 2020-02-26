b12223a7cfa37a78cbd195b0d011565812f99294
root <root@promote.cache-dns.local>
2020-02-26 13:03:42 +0800

    homework2

---
+++ LeetCode141.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: LeetCode141.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月26日 星期三 13时03分32秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode *p = head,*q = head;
+    if(p == NULL)
+    return false;
+    do{
+        p = p->next;
+        q = q->next;
+        if(q == NULL || q->next == NULL)
+        return false;
+        q = q->next;
+    }while(p != q);
+    return true;
+}
